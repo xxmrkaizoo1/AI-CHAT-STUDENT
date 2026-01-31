2aee951498057f3bbe9276f7acf13692c93ada73
xxmrkaizoo1 <firasumairisgood@gmail.com>
2026-02-01 00:59:12 +0800

    Npm  install breeze to laravel

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChatController;
-use App\Http\Controllers\TeacherChatController;
 
-Route::get('/chat', [ChatController::class, 'index']);
-Route::post('/chat', [ChatController::class, 'send']);
-Route::post('/chat/clear', [ChatController::class, 'clear']);
-
-
-
-
-Route::get('/teacher/chats', [TeacherChatController::class, 'index']);
-Route::get('/teacher/chats/{sessionId}', [TeacherChatController::class, 'show']);
-
-
-
-Route::get('/ai-test', function () {
-    $res = Http::withOptions(['force_ip_resolve' => 'v4'])
-        ->connectTimeout(100)
-        ->timeout(120)
-        ->asJson()
-        ->post(env('OLLAMA_URL') . '/api/generate', [
-            'model' => env('OLLAMA_MODEL'),
-            'prompt' => 'can u  speak  100 words ',
-            'stream' => false,
-            'options' => [
-                'num_predict' => 100,
-
-                // VERY HIGH → slower test
-                // VERY SMALL → fastest test
-            ],
-        ]);
-
-    return $res->json();
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
